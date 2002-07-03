7f62d4c0d5a252ae524a70e5b44514399389700b
phase1geo <>
2002-07-03 00:58:17 +0000

    Adding first conditional statement diagnostic.  This diagnostic now passes.

---
+++ diags/verilog/cond1.v
@@ -0,0 +1,28 @@
+module main;
+
+wire    a;
+reg     b;
+reg     c, d;
+
+assign a = b ? c : d;
+
+initial begin
+        $dumpfile( "cond1.vcd" );
+        $dumpvars( 0, main );
+        b = 1'b1;
+        c = 1'b0;
+        d = 1'b1;
+	#5;
+        c = 1'b1;
+        d = 1'b0;
+        #5;
+        b = 1'b0;
+        #5;
+        c = 1'b0;
+        d = 1'b1;
+	#5;
+	b = 1'b1;
+	#5;
+end
+
+endmodule
