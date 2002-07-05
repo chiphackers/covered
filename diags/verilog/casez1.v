4b7eced86dcc4b6726d5d4db9c185f8009a7da12
phase1geo <>
2002-07-05 04:45:10 +0000

    Adding diagnostics for casez.  Full regression passes.

---
+++ diags/verilog/casez1.v
@@ -0,0 +1,28 @@
+module main;
+
+reg	a, b;
+
+always @( a )
+  casez( a )
+    1'b0 :  b = 1'b1;
+    1'b1 :  b = 1'b0;
+    1'bx :  b = 1'b1;
+    1'bz :  b = 1'b0;
+  endcase
+
+initial begin
+	$dumpfile( "casez1.vcd" );
+	$dumpvars( 0, main );
+	#5;
+	a = 1'b0;
+	#5;
+	a = 1'b1;
+	#5;
+	a = 1'bx;
+	#5;
+	a = 1'bz;
+	#5;
+	$finish;
+end
+
+endmodule
