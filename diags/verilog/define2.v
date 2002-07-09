3520f91c8f97ce8f96464119bc7ebbeb2fcf4ae2
phase1geo <>
2002-07-09 23:16:34 +0000

    Added defines2.v diagnostic to verify nested defines work properly. Diagnostic passes.

---
+++ diags/verilog/define2.v
@@ -0,0 +1,24 @@
+module main;
+
+`define VALUE0    1'b0
+`define VALUE1    1'b1
+`define VALUE_AND (`VALUE0 & `VALUE1)
+`define VALUE_OR  (`VALUE0 | `VALUE1)
+
+wire     a, b;
+reg      sel;
+
+assign a = sel ? `VALUE0 : `VALUE1;
+assign b = sel ? `VALUE_AND : `VALUE_OR;
+
+initial begin
+	$dumpfile( "define2.vcd" );
+	$dumpvars( 0, main );
+	sel = 1'b0;
+	#10;
+	sel = 1'b1;
+	#10;
+	$finish;
+end
+
+endmodule
