1dd089be00a7a0a739a925d0a38f4cf495d3a71b
phase1geo <>
2002-08-26 12:57:43 +0000

    Adding defparam diagnostic to verify that Covered does not handle defparam statements.

---
+++ diags/verilog/defparam1.v
@@ -0,0 +1,36 @@
+module main;
+
+reg  in;
+wire out;
+
+initial begin
+        $dumpfile( "defparam1.vcd" );
+        $dumpvars( 0, main );
+	in = 1'b0;
+	#10;
+	in = 1'b1;
+	#10;
+	in = 1'b0;
+	#100;
+	$finish;
+end
+
+defparam foo.a = 1'b1;
+
+foobar foo(
+  .y( in  ),
+  .x( out )
+);
+
+endmodule
+
+module foobar( y, x );
+
+input  y;
+output x;
+
+parameter a = 1'b0;
+
+assign x = y & a;
+
+endmodule
