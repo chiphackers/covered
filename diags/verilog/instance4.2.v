f3a9b38ac85a61bce6f21368b7e609477718443e
phase1geo <>
2002-10-13 19:37:04 +0000

    Adding more diagnostics to check proper instance handling.  Instances now look pretty good when good options are specified.  Diagnostics add to full regression.

---
+++ diags/verilog/instance4.2.v
@@ -0,0 +1,44 @@
+module main;
+
+reg    a, b;
+
+foobar foo( a, b );
+
+initial begin
+	$dumpfile( "instance4.vcd" );
+	$dumpvars( 0, main );
+	a = 1'b0;
+	b = 1'b0;
+	#5;
+	a = 1'b1;
+	#5;
+	a = 1'b0;
+	b = 1'b1;
+	#5;
+	$finish;
+end
+
+endmodule
+
+
+module foobar( b, c );
+
+input  b, c;
+wire   a;
+
+assign a = b | c;
+
+fooman foo( b, c );
+
+endmodule
+
+
+module fooman( b, c );
+
+input   b;
+input   c;
+
+wire   a;
+assign a = b & c;
+
+endmodule
