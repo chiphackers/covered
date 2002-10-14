277258f2db0f5e078ce45b11150f742e05e86dd1
phase1geo <>
2002-10-14 18:46:44 +0000

    Adding test case that causes assertion error in Covered score command.

---
+++ diags/verilog/test.v
@@ -0,0 +1,25 @@
+module main;
+
+wire   [129:0]  a;
+reg		b;
+reg		c;
+reg		d;
+reg    [127:0]  e;
+reg		f, g;
+
+assign a = {130{b & c}} & {d, 1'b0, e[63:0], e[63:0]} | {130{c & ~b}} & {f, g, e[127:0]};
+
+initial begin
+	$dumpfile( "test.vcd" );
+	$dumpvars( 0, main );
+	b = 1'b1;
+	c = 1'b0;
+	d = 1'b0;
+	e = 128'h0123456789abcdeffedcba9876543210;
+        f = 1'b0;
+	g = 1'b0;
+	#5;
+	$finish;
+end
+
+endmodule
