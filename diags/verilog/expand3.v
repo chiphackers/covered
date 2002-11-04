26d6675c5878801d1417564fa966d3b225c78109
phase1geo <>
2002-11-04 19:27:29 +0000

    Adding expand3.v diagnostic which shows a problem with handling expand expressions where the multiplier is an expression.  Added diagnostic to full regression list.

---
+++ diags/verilog/expand3.v
@@ -0,0 +1,19 @@
+module main;
+
+wire [3:0] a;
+reg        b;
+
+assign a = {5-1{b}};
+
+initial begin
+	$dumpfile( "expand3.vcd" );
+	$dumpvars( 0, main );
+	#5;
+	b = 1'b0;
+	#5;
+	b = 1'bz;
+	#5;
+	$finish;
+end
+
+endmodule
