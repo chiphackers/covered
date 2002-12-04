cfae40845b942af3c1a686245a47c393bfa1aa18
phase1geo <>
2002-12-04 22:52:12 +0000

    Adding instance6.1.v diagnostic which hits bug found in example codes.

---
+++ diags/verilog/instance6.1.v
@@ -0,0 +1,49 @@
+/* 
+ * Verifies the case where we have already parsed the needed module
+ * and then hit another instance that instantiates the module again.
+ */
+
+module main;
+
+reg [2:0] a, b;
+
+foobar foo( a );
+barfoo bar( b );
+
+initial begin
+	$dumpfile( "instance6.1.vcd" );
+	$dumpvars( 0, main );
+	a = 3'b000;
+        b = 3'b111;
+	#5;
+	a = 3'b001;
+	#5;
+	a = 3'b100;
+	#5;
+	$finish;
+end
+
+endmodule
+
+
+module foobar( b );
+
+parameter bsize = 3;
+
+input [bsize-1:0]  b;
+
+wire [1:0] a;
+
+assign a = b[2:1];
+
+endmodule
+
+
+module barfoo( b );
+
+input [2:0]  b;
+
+foobar dude( b );
+
+endmodule
+
