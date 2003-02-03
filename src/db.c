6d487aee382401b9a1963c11f128668fe63bb8a5
phase1geo <>
2003-02-03 17:17:38 +0000

    Fixing bug with statement deallocation for NULL statements.

--- src/db.c
+++ src/db.c
@@ -808,14 +808,18 @@ void db_add_statement( statement* stmt, statement* start ) {
 */
 void db_remove_statement( statement* stmt ) {
 
-  /* Remove expression from current module expression list */
-  exp_link_remove( stmt->exp, &(curr_module->exp_head), &(curr_module->exp_tail) );
+  if( stmt != NULL ) {
 
-  /* Deallocate expression tree for this statement */
-  expression_dealloc( stmt->exp, FALSE );
+    /* Remove expression from current module expression list */
+    exp_link_remove( stmt->exp, &(curr_module->exp_head), &(curr_module->exp_tail) );
 
-  /* Deallocate statement itself */
-  statement_dealloc( stmt );
+    /* Deallocate expression tree for this statement */
+    expression_dealloc( stmt->exp, FALSE );
+
+    /* Deallocate statement itself */
+    statement_dealloc( stmt );
+
+  }
 
 }
 
@@ -1112,6 +1116,10 @@ void db_do_timestep( int time ) {
 
 /*
  $Log$
+ Revision 1.80  2003/01/25 22:39:02  phase1geo
+ Fixing case where statement is found to be unsupported in middle of statement
+ tree.  The entire statement tree is removed from consideration for simulation.
+
  Revision 1.79  2003/01/06 00:44:21  phase1geo
  Updates to NEWS, ChangeLog, development documentation and user documentation
  for new 0.2pre1_20030105 release.
