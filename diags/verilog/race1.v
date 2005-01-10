072d6468dc655d3b910ca01638453d9d1292c8a9
phase1geo <>
2005-01-10 13:48:10 +0000

    Adding race1 test into testsuite (though this is not part of the regression suite yet).  Updating CDD files for latest changes.

---
+++ diags/verilog/race1.v
@@ -0,0 +1,23 @@
+module main;
+
+wire a;
+
+foobar bar( a );
+
+initial begin
+	$dumpfile( "race1.vcd" );
+	$dumpvars( 0, main );
+	#10;
+	$finish;
+end
+
+endmodule
+
+
+module foobar ( a );
+
+input a;
+
+assign a = 1'b0;
+
+endmodule
