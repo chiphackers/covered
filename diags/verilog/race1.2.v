8a75459567fbc93f00ce829e23e9211a214d7098
phase1geo <>
2005-01-25 13:59:21 +0000

    Adding race1.2 to regression suite.

---
+++ diags/verilog/race1.2.v
@@ -0,0 +1,25 @@
+module main;
+
+wire       a;
+wire [1:0] c;
+wire [2:0] d;
+reg        b;
+
+assign a = b;
+assign a = ~b;
+
+assign c[0] = b;
+assign c[1] = ~b;
+
+assign d[1:0] = c;
+assign d[2:1] = ~c;
+
+initial begin
+	$dumpfile( "race1.2.vcd" );
+	$dumpvars( 0, main );
+	b = 1'b0;
+	#10;
+	$finish;
+end
+
+endmodule
