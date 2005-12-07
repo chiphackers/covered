7e97358e80916e71fd0cc5731b0c463746557f2b
phase1geo <>
2005-12-07 22:02:35 +0000

    Added new repeat2 diagnostic to regression suite.  This brings out a statement_connect error that still needs to be looked at -- currently causes just this diagnostic to segfault.

---
+++ diags/verilog/repeat2.v
@@ -0,0 +1,32 @@
+module main;
+
+reg        a, b;
+reg [31:0] c;
+
+initial begin
+	a = 1'b0;
+	#10;
+	repeat( c )
+	  begin
+	   a = ~a;
+           @(b);
+          end
+	a = ~a;
+end
+
+initial begin
+        $dumpfile( "repeat2.vcd" );
+        $dumpvars( 0, main );
+        b = 1'b0;
+	c = 10;
+        #10;
+	b = 1'b1;
+	#10;
+	b = 1'b0;
+	#10;
+	b = 1'b1;
+	#10;
+        $finish;
+end
+
+endmodule
