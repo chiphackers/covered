bd6cb8cfdc866bea3d94e3940475f841ddaffe94
phase1geo <>
2006-01-12 23:14:26 +0000

    Added test to verify defparam overridding of localparams cannot occur.  Updated TODO file to list which Verilog-2001 constructs are currently supported.  Full regression passes.

---
+++ diags/verilog/localparam3.v
@@ -0,0 +1,21 @@
+module main;
+
+foo bar();
+
+initial begin
+        $dumpfile( "localparam3.vcd" );
+        $dumpvars( 0, main );
+        #10;
+        $finish;
+end
+
+endmodule
+
+
+module foo;
+
+localparam SIZE = 5;
+
+wire [(SIZE-1):0] a;
+
+endmodule
