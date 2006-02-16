cb3cb5eadc546dc4780a9e35ff6c33b4099da150
phase1geo <>
2006-02-16 05:33:15 +0000

    More additions to regression script.  Need to do lots of debugging.  add1.v diagnostic contains initial support for new regression style.

--- diags/verilog/add1.v
+++ diags/verilog/add1.v
@@ -15,3 +15,15 @@ initial begin
 end
 
 endmodule
+
+/* HEADER
+GROUPS add1 iv vcs vcd lxt
+COMP   iv : iverilog add1.v
+COMP   vcs : vcs add1.v
+SIM    iv : ./a.out
+SIM    lxt : ./a.out -lxt2
+SIM    vcs : ./simv
+SCORE  add1 iv vcs vcd : -t main -vcd add1.vcd -v add1.v -o add1.cdd
+SCORE  add1 iv lxt : -t main -lxt add1.vcd -v add1.v -o add1.cdd
+REPORT add1 iv vcs vcd lxt : -d v -o add1.v.rpt add1.cdd
+*/
