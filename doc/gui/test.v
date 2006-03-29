0442f83f2f67fa8b08ea34f6843e7c99fd35c9d4
phase1geo <>
2006-03-29 18:06:26 +0000

    More GUI documentation updates.  This should do it for the updates for now.  We just need to run the autoreconf and automake tools.

--- doc/gui/test.v
+++ doc/gui/test.v
@@ -1,6 +1,6 @@
 module main;
 
-reg a, b, c, d, e, f, g, h, i, j;
+reg a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
 reg clock;
 reg [3:0] err_vec;
 
@@ -21,7 +21,17 @@ end
 
 initial begin
 	#1;
-	g = h | i | j;
+	g = h | i;
+end
+
+initial begin
+	#1;
+	j = k ^ l;
+end
+
+initial begin
+	#1;
+	m = n | o | p;
 end
 
 initial begin
@@ -30,9 +40,13 @@ initial begin
 	c = 1'b0;
 	e = 1'b1;
 	f = 1'b0;
-	h = 1'b0;
-	i = 1'b1;
-	j = 1'b1;
+        h = 1'b1;
+        i = 1'b0;
+        k = 1'b0;
+        l = 1'b1;
+	n = 1'b0;
+	o = 1'b1;
+	p = 1'b1;
 	err_vec = 4'b0000;
 	#5;
 	err_vec = 4'b1100;
