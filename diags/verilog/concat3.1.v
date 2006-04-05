87809bd7fd1679188342ffbd538b1aecbdb30b3e
phase1geo <>
2006-04-05 04:20:10 +0000

    Fixing bug expression_resize function to properly handle the sizing of a concatenation on the left-hand-side of an expression.  Added diagnostic to regression suite to duplicate this scenario and verify the fix.

---
+++ diags/verilog/concat3.1.v
@@ -0,0 +1,20 @@
+module main;
+
+wire [1:0] a, b;
+reg  [3:0] c, d;
+
+assign {a, b} = c & d;
+
+initial begin
+        $dumpfile( "concat3.1.vcd" );
+        $dumpvars( 0, main );
+	c = 4'b0000;
+	d = 4'b0000;
+	#10;
+	c = 4'b0111;
+	d = 4'b0110;
+        #10;
+        $finish;
+end
+
+endmodule
--- src/expr.c
+++ src/expr.c
@@ -726,6 +726,7 @@ void expression_resize( expression* expr, bool recursive ) {
 
       default :
         if( (ESUPPL_IS_ROOT( expr->suppl ) == 1) ||
+            (ESUPPL_IS_LHS( expr->suppl ) == 1) ||
             ((expr->parent->expr->op != EXP_OP_ASSIGN) &&
              (expr->parent->expr->op != EXP_OP_DASSIGN) &&
              (expr->parent->expr->op != EXP_OP_BASSIGN) &&
@@ -3134,6 +3135,11 @@ void expression_dealloc( expression* expr, bool exp_only ) {
 
 /* 
  $Log$
+ Revision 1.178  2006/03/28 22:28:27  phase1geo
+ Updates to user guide and added copyright information to each source file in the
+ src directory.  Added test directory in user documentation directory containing the
+ example used in line, toggle, combinational logic and FSM descriptions.
+
  Revision 1.177  2006/03/27 23:25:30  phase1geo
  Updating development documentation for 0.4 stable release.
 
