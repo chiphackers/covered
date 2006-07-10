774a1cf429bfa8adc26380ab8b19f1f8e86d3562
phase1geo <>
2006-07-10 22:39:04 +0000

    Adding generate1 diagnostic which just tests a simple generate loop.

---
+++ diags/verilog/generate1.v
@@ -0,0 +1,22 @@
+module main;
+
+generate
+  genvar i;
+  for( i=0; i<4; i=i+1 )
+    begin : U
+      reg [1:0] a;
+    end 
+endgenerate
+
+initial begin
+        $dumpfile( "generate1.vcd" );
+        $dumpvars( 0, main );
+	//U[0].a = 2'h0;
+	//U[1].a = 2'h1;
+	//U[2].a = 2'h2;
+	//U[3].a = 2'h3;
+        #10;
+        $finish;
+end
+
+endmodule
