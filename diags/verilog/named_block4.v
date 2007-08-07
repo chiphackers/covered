2a70dcdb7857c5989b7e0b49ca126e603787a5c1
phase1geo <>
2007-08-07 01:54:54 +0000

    Adding named_block4 diagnostic to regression suite to verify bug fix.  Also adding AC_SYS_LARGEFILE to configure.in and rebuilding configure with this change to properly support reading in files larger than 2GB.

---
+++ diags/verilog/named_block4.v
@@ -0,0 +1,32 @@
+/*
+ Name:     named_block4.v
+ Author:   Trevor Williams  (trevorw@charter.net)
+ Date:     08/06/2007
+ Purpose:  Used to verify fix for bug 1621096.
+*/
+
+module main;
+
+reg a, b;
+
+always
+  begin : foo
+    #5;
+    a = b;
+  end
+
+initial begin
+`ifndef VPI
+        $dumpfile( "named_block4.vcd" );
+        $dumpvars( 0, main );
+`endif
+	b = 1'b0;
+	#6;
+	b = 1'b1;
+	#5;
+	b = 1'b0;
+        #10;
+        $finish;
+end
+
+endmodule
