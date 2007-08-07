46e21a7c895b8389e4ae2f7716e878dc4dbe0559
phase1geo <>
2007-08-07 03:42:50 +0000

    Adding ability to enter defined values inside of covered_fsm attributes (to match the documentation).  Fix for bug 1709984.  Added fsm11 diagnostic to verify this new ability.

---
+++ diags/verilog/fsm11.v
@@ -0,0 +1,34 @@
+/*
+ Name:     fsm11.v
+ Author:   Trevor Williams  (trevorw@charter.net)
+ Date:     08/06/2007
+ Purpose:  Verify usage of defined values in FSM attribute list.
+*/
+
+`define ZERO 1'b0
+`define ONE  1'b1
+
+module main;
+
+reg a, b;
+
+(* covered_fsm, foo, is="a", os="b",
+   trans="`ONE->`ZERO",
+   trans="1'b0->1'b1" *)
+always @(a)
+  case( a )
+    `ZERO :  b = `ONE;
+    `ONE  :  b = `ZERO;
+  endcase
+
+initial begin
+`ifndef VPI
+        $dumpfile( "fsm11.vcd" );
+        $dumpvars( 0, main );
+`endif
+	a = 1'b0;
+        #10;
+        $finish;
+end
+
+endmodule
