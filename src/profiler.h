44070aba37e85744f4201cc5d1823f5d64a8d795
phase1geo <>
2007-12-11 15:07:35 +0000

    More modifications.

--- src/profiler.h
+++ src/profiler.h
@@ -40,13 +40,16 @@
 #undef PROFILE_TIME_STOP
 #undef MALLOC_CALL
 #undef FREE_CALL
-#define PROFILE(index)     unsigned int profile_index = index;  profiles[index].calls++;
-#define PROFILE_TIME_START timer_start(&profiles[profile_index].time_in);
-#define PROFILE_TIME_STOP  timer_stop(&profiles[profile_index].time_in);
-#define MALLOC_CALL(index) profiles[index].mallocs++;
-#define FREE_CALL(index)   profiles[index].frees++;
+#define PROFILE(index)     unsigned int profile_index = index;  if(profiling_mode) profiles[index].calls++;
+#define PROFILE_TIME_START if(profiling_mode) timer_start(&profiles[profile_index].time_in);
+#define PROFILE_TIME_STOP  if(profiling_mode) timer_stop(&profiles[profile_index].time_in);
+#define MALLOC_CALL(index) if(profiling_mode) profiles[index].mallocs++;
+#define FREE_CALL(index)   if(profiling_mode) profiles[index].frees++;
 #endif
 #endif
+
+
+extern bool profiling_mode;
 
 
 /*! \brief Sets the current profiling mode to the given value. */
@@ -61,6 +64,11 @@ void profiler_report();
 
 /*
  $Log$
+ Revision 1.2  2007/12/11 05:48:26  phase1geo
+ Fixing more compile errors with new code changes and adding more profiling.
+ Still have a ways to go before we can compile cleanly again (next submission
+ should do it).
+
  Revision 1.1  2007/12/10 23:16:22  phase1geo
  Working on adding profiler for use in finding performance issues.  Things don't compile
  at the moment.
