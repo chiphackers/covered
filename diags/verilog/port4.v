c5faeff70ca44cc612b705f0c62303d705c30491
phase1geo <>
2007-12-31 23:46:55 +0000

    Adding new port4 diagnostic to verify fix for bug 1858408.

---
+++ diags/verilog/port4.v
@@ -0,0 +1,39 @@
+/*
+ Name:     port4.v
+ Author:   Trevor Williams  (phase1geo@gmail.com)
+ Date:     12/31/2007
+ Purpose:  Verify that function input port is sized correctly.
+*/
+
+module main;
+
+integer a;
+wire [31:0] b;
+
+foo foo_mod(a,b);
+
+initial begin
+`ifndef VPI
+        $dumpfile( "port4.vcd" );
+        $dumpvars( 0, main );
+`endif
+	a = 0;
+	#5;
+	a = 10;
+        #5;
+        $finish;
+end
+
+endmodule
+
+
+module foo(a,b);
+
+input a;
+output [31:0] b;
+
+wire [31:0] a;
+
+assign b = a;
+
+endmodule
