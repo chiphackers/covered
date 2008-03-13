866bb0d2bd2832220ce332335ee144f14dcc29a7
phase1geo <>
2008-03-13 23:20:50 +0000

    Updating null_stmt diagnostics from stable branch.

---
+++ diags/verilog/null_stmt1.1.v
@@ -0,0 +1,34 @@
+/*
+ Name:        null_stmt1.1.v
+ Author:      Trevor Williams  (phase1geo@gmail.com)
+ Date:        03/13/2008
+ Purpose:     Verify that null statement in the true block of an if..then..else
+              block works properly.
+ Simulators:  IV CVER VERIWELL VCS
+ Modes:       VCD LXT VPI
+*/
+
+module main;
+
+reg a, b;
+
+initial begin
+	a = 1'b1;
+	b = 1'b0;
+	if( a )
+	  ;
+	else
+	  b = 1'b1;
+	a = 1'b0;
+end
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "null_stmt1.1.vcd" );
+        $dumpvars( 0, main );
+`endif
+        #10;
+        $finish;
+end
+
+endmodule
---
+++ diags/verilog/null_stmt1.2.v
@@ -0,0 +1,33 @@
+/*
+ Name:        null_stmt1.2.v
+ Author:      Trevor Williams  (phase1geo@gmail.com)
+ Date:        03/13/2008
+ Purpose:     
+ Simulators:  IV CVER VERIWELL VCS
+ Modes:       VCD LXT VPI
+*/
+
+module main;
+
+reg a, b;
+
+initial begin
+	a = 1'b0;
+	b = 1'b0;
+	if( a )
+	  b = 1'b1;
+	else
+	  ;
+	a = 1'b1;
+end
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "null_stmt1.2.vcd" );
+        $dumpvars( 0, main );
+`endif
+        #10;
+        $finish;
+end
+
+endmodule
