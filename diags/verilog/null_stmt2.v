ecce6f18f4a38bd4c103217ece2a7f184ed7ea75
phase1geo <>
2008-06-02 23:25:16 +0000

    Initial versions of diagnostics to verify null statements in statement lists.

---
+++ diags/verilog/null_stmt2.v
@@ -0,0 +1,30 @@
+/*
+ Name:        null_stmt2.v
+ Author:      Trevor Williams  (phase1geo@gmail.com)
+ Date:        06/02/2008
+ Purpose:     Verifies that a null statement can exist in a statement block.
+ Simulators:  IV CVER VERIWELL VCS
+ Modes:       VCD LXT VPI
+*/
+
+module main;
+
+reg a;
+
+initial begin
+	a = 1'b0;;
+	#5;
+	;
+	a = 1'b1;;;;
+end
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "null_stmt2.vcd" );
+        $dumpvars( 0, main );
+`endif
+        #10;
+        $finish;
+end
+
+endmodule
