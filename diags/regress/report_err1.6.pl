324234596be79b897a54740260fca01236e81a90
phase1geo <>
2008-06-20 13:02:25 +0000

    Adding report_err1.6 to regression suite.

---
+++ diags/regress/report_err1.6.pl
@@ -0,0 +1,19 @@
+# Name:     report_err1.6.pl
+# Author:   Trevor Williams  (phase1geo@gmail.com)
+# Date:     06/19/2008
+# Purpose:  Verifies that a bad report command is reported as such.
+
+require "../verilog/regress_subs.pl";
+
+# Initialize the diagnostic environment
+&initialize( "report_err1.6", 1, @ARGV );
+
+# Run the report command
+&runReportCommand( "-d v -x -o report_err1.6.rpt report_err1.6.cdd 2> report_err1.6.err" );
+system( "cat report_err1.6.err" ) && die;
+
+# Perform the file comparison checks
+&checkTest( "report_err1.6", 1, 1 );
+
+exit 0;
+
