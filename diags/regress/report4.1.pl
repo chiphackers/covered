1b14729edc2173ccece7bad38cc08625c4107a18
phase1geo <>
2008-06-20 02:18:56 +0000

    Adding report4.1 diagnostic to regression suite.

---
+++ diags/regress/report4.1.pl
@@ -0,0 +1,32 @@
+# Name:     report4.1.pl
+# Author:   Trevor Williams  (phase1geo@gmail.com)
+# Date:     06/19/2008
+# Purpose:  Verifies that the -d d option works correctly.
+
+require "../verilog/regress_subs.pl";
+
+# Initialize the diagnostic environment
+&initialize( "report4.1", 0, @ARGV );
+
+# Simulate and get coverage information
+if( $SIMULATOR eq "IV" ) { 
+  system( "iverilog -DDUMP report4.1.v; ./a.out" ) && die;
+} elsif( $SIMULATOR eq "CVER" ) {
+  system( "cver +define+DUMP report4.1.v" ) && die;
+} elsif( $SIMULATOR eq "VCS" ) {
+  system( "vcs +define+DUMP report4.1.v; ./simv" ) && die;
+}
+
+&runScoreCommand( "-t main -vcd report4.1.vcd -v report4.1.v -o report4.1.cdd" );
+&runReportCommand( "-d d -m ltcfam -o report4.1.rptM report4.1.cdd" );
+&runReportCommand( "-d d -m ltcfam -i -o report4.1.rptI report4.1.cdd" );
+
+# Perform the file comparison checks
+if( $DUMPTYPE eq "VCD" ) { 
+  &checkTest( "report4.1", 1, 0 );
+} else {
+  &checkTest( "report4.1", 1, 5 );
+}
+
+exit 0;
+
