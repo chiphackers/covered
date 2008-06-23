896de201b542c94bbb489161c39d1810a068c8ed
phase1geo <>
2008-06-23 02:33:39 +0000

    Adding err9 diagnostic to regression suite.

---
+++ diags/regress/err9.pl
@@ -0,0 +1,48 @@
+# Name:     err9.pl
+# Author:   Trevor Williams  (phase1geo@gmail.com)
+# Date:     06/22/2008
+# Purpose:  Hit a compatibility problem with merged CDD file and verify that
+#           an appropriate error message to the user.
+
+require "../verilog/regress_subs.pl";
+
+# Initialize the diagnostic environment
+&initialize( "err9", 1, @ARGV );
+
+# Simulate and get coverage information
+if( $SIMULATOR eq "IV" ) { 
+  system( "iverilog -DDUMP err9.v; ./a.out" ) && die;
+} elsif( $SIMULATOR eq "CVER" ) {
+  system( "cver +define+DUMP err9.v" ) && die;
+} elsif( $SIMULATOR eq "VCS" ) {
+  system( "vcs +define+DUMP err9.v; ./simv" ) && die;
+}
+
+# Create scored CDD file
+&runScoreCommand( "-t main -vcd err9.vcd -v err9.v -o err9.tmp.cdd" );
+
+# Modify the CDD file to cause a merged syntax error
+open( OLD_CDD, "err9.tmp.cdd" ) || die "Can't open err9.tmp.cdd for reading: $!\n";
+open( NEW_CDD, ">err9.cdd" ) || die "Can't open err9.cdd for writing: $!\n";
+while( <OLD_CDD> ) {
+  chomp;
+  if( /^5\s+(\w+)\s+(\w+)\s+(.+)\s+.*$/ ) {
+    $version = $1;
+    $suppl   = $2;
+    $lhier   = $3;
+    print NEW_CDD "5 $version $suppl $lhier 2\n";
+  } else {
+    print NEW_CDD $_ . "\n";
+  }
+}
+close( NEW_CDD );
+close( OLD_CDD );
+system( "rm -f err9.tmp.cdd" ) && die;
+&runReportCommand( "-d v -o err9.rptM err9.cdd 2> err9.err" );
+system( "cat err9.err" ) && die;
+
+# Perform the file comparison checks
+&checkTest( "err9", 1, 1 );
+
+exit 0;
+
--- src/info.c
+++ src/info.c
@@ -219,7 +219,6 @@ void info_db_read( char** line ) { PROFILE(INFO_DB_READ);
 
       } else {
         print_output( "CDD file being read is incompatible with this version of Covered", FATAL, __FILE__, __LINE__ );
-        printf( "info Throw B\n" );
         Throw 0;
       }
 
@@ -308,6 +307,9 @@ void info_dealloc() { PROFILE(INFO_DEALLOC);
 
 /*
  $Log$
+ Revision 1.34  2008/06/22 22:02:01  phase1geo
+ Fixing regression issues.
+
  Revision 1.33  2008/06/19 16:14:55  phase1geo
  leaned up all warnings in source code from -Wall.  This also seems to have cleared
  up a few runtime issues.  Full regression passes.
