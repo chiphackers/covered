21855d2314a21b036615ec8ec0542f869dd830ee
phase1geo <>
2008-09-17 06:16:31 +0000

    Adding merge6.1 diagnostic to regression suite.

---
+++ diags/regress/merge6.1.pl
@@ -0,0 +1,90 @@
+# Name:     merge6.1.pl
+# Author:   Trevor Williams  (phase1geo@gmail.com)
+# Date:     08/16/2008
+# Purpose:  Verifies that a two-level merge works properly even when a file
+#           is named the same in two different directories.
+
+require "../verilog/regress_subs.pl";
+
+# Initialize the diagnostic environment
+&initialize( "merge6.1", 0, @ARGV );
+
+# First, create the two subdirectories that will contain the partially merged data contents.
+system( "mkdir -p merge6_1 merge6_2" ) && die;
+
+# Run all of the CDDs to be merged
+$retval = &run( "merge6.1a", 1 ) || $retval;
+$retval = &run( "merge6.1b", 0 ) || $retval;
+$retval = &run( "merge6.1c", 1 ) || $retval;
+$retval = &run( "merge6.1d", 0 ) || $retval;
+
+# Save the value of CHECK_MEM_CMD
+$ORIG_CHECK_MEM_CMD = $CHECK_MEM_CMD;
+
+# Set the covered and check_mem paths accordingly
+$COVERED = "../../../src/covered";
+if( $ORIG_CHECK_MEM_CMD ne "" ) {
+  $CHECK_MEM_CMD = "| ../check_mem";
+}
+
+# Perform merge command of first two CDDs
+chdir( "merge6_1" ) || die "Unable to change to merge6_1 directory: $!\n";
+&runMergeCommand( "-o merge6.1.cdd merge6.1a.cdd merge6.1b.cdd" );
+
+# Perform merge command of last two CDDs
+chdir( "../merge6_2" ) || die "Unable to change to merge6_2 directory: $!\n";
+&runMergeCommand( "-o merge6.1.cdd merge6.1a.cdd merge6.1b.cdd" );
+
+# Perform top-most merge
+chdir( ".." ) || die "Unable to change to main directory: $!\n";
+$COVERED       = "../../src/covered";
+$CHECK_MEM_CMD = $ORIG_CHECK_MEM_CMD;
+&runMergeCommand( "-o merge6.1.cdd merge6_1/merge6.1.cdd merge6_2/merge6.1.cdd" );
+
+# Remove the temporary directories
+system( "rm -rf merge6_1 merge6_2" ) && die;
+
+# Now perform report commands
+&runReportCommand( "-d v -o merge6.1.rptM merge6.1.cdd" );
+&runReportCommand( "-d v -i -o merge6.1.rptI merge6.1.cdd" );
+
+# Perform the file comparison checks
+&checkTest( "merge6.1", 5, 0 );
+
+exit 0;
+
+sub run {
+
+  my( $bname, $d ) = @_;
+  my( $retval ) = 0;
+
+  # Simulate the design
+  if( $SIMULATOR eq "IV" ) {
+    $def = ($d == 1) ? "-DTEST1" : "";
+    system( "iverilog -DDUMP $def -y lib ${bname}.v; ./a.out" ) && die; 
+  } elsif( $SIMULATOR eq "CVER" ) {
+    $def = ($d == 1) ? "+define+TEST1" : "";
+    system( "cver -q +define+DUMP $def +libext+.v+ -y lib ${bname}.v" ) && die;
+  } elsif( $SIMULATOR eq "VCS" ) {
+    $def = ($d == 1) ? "+define+TEST1" : "";
+    system( "vcs +define+DUMP $def +v2k -sverilog +libext+.v+ -y lib ${bname}.v; ./simv" ) && die; 
+  } else {
+    die "Illegal SIMULATOR value (${SIMULATOR})\n";
+  }
+
+  # Convert configuration file
+  if( $DUMPTYPE eq "VCD" ) {
+    &convertCfg( "vcd", "${bname}.cfg" );
+  } elsif( $DUMPTYPE eq "LXT" ) {
+    &convertCfg( "lxt", "${bname}.cfg" );
+  } else {
+    die "Illegal DUMPTYPE value (${DUMPTYPE})\n";
+  }
+
+  # Score CDD file
+  &runScoreCommand( "-f ${bname}.cfg -D DUMP" );
+
+  return $retval;
+
+}
+
---
+++ diags/verilog/merge6.1b.v
@@ -0,0 +1,35 @@
+/*
+ Name:        merge6.1b.v
+ Author:      Trevor Williams  (phase1geo@gmail.com)
+ Date:        09/15/2008
+ Purpose:     See ../regress/merge6.1.pl for details
+*/
+
+module main;
+
+wire a;
+reg  b, c;
+
+dut_and a(
+  .a(a),
+  .b(b),
+  .c(c)
+);
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "merge6.1b.vcd" );
+        $dumpvars( 0, main );
+`endif
+`ifdef TEST1
+	b = 1'b1;
+	c = 1'b0;
+`else
+	b = 1'b1;
+	c = 1'b1;
+`endif
+        #10;
+        $finish;
+end
+
+endmodule
