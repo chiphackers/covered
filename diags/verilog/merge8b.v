3536f52fbb05b36dfbaa31b9f2bf89177e22b4d7
phase1geo <>
2008-10-31 13:23:18 +0000

    Adding diagnostic to verify merging seperate pieces of the same design.

---
+++ diags/verilog/merge8b.v
@@ -0,0 +1,47 @@
+/*
+ Name:        merge8.v
+ Author:      Trevor Williams  (phase1geo@gmail.com)
+ Date:        10/31/2008
+ Purpose:     Verifies that different modules from the same design can be merged.
+*/
+
+module main;
+
+reg a, b;
+
+foo f( a, b );
+bar b( b );
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "merge8.vcd" );
+        $dumpvars( 0, main );
+`endif
+	a = 1'b0;
+	b = 1'b1;
+        #10;
+        $finish;
+end
+
+endmodule
+
+//-------------------------------------------------
+
+module foo(
+  input wire a,
+  input wire b
+);
+
+wire c = a | b;
+
+endmodule
+
+//-------------------------------------------------
+
+module bar(
+  input wire a
+);
+
+wire b = ~a;
+
+endmodule
