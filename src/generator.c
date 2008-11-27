64b2f2137a49e8d890f9495c03682a097d00c190
phase1geo <>
2008-11-27 00:01:50 +0000

    More work on coverage generator.  Checkpointing.

--- src/generator.c
+++ src/generator.c
@@ -22,6 +22,7 @@
 #include "defines.h"
 #include "func_iter.h"
 #include "generator.h"
+#include "link.h"
 #include "profiler.h"
 #include "util.h"
 
@@ -31,59 +32,230 @@ extern unsigned int curr_db;
 extern char         user_msg[USER_MSG_LENGTH];
 
 
+struct fname_link_s;
+typedef struct fname_link_s fname_link;
+struct fname_link_s {
+  char*       filename;    /*!< Filename associated with all functional units */
+  func_unit*  next_funit;  /*!< Pointer to the next/current functional unit that will be parsed */
+  funit_link* head;        /*!< Pointer to head of functional unit list */
+  funit_link* tail;        /*!< Pointer to tail of functional unit list */
+  fname_link* next;        /*!< Pointer to next filename list */
+};
+
+/*!
+ Populates the specified filename list with the functional unit list, sorting all functional units with the
+ same filename together in the same link.
+*/
+static void generator_create_filename_list(
+  funit_link*  funitl,  /*!< Pointer to the head of the functional unit linked list */
+  fname_link** head,    /*!< Pointer to the head of the filename list to populate */
+  fname_link** tail     /*!< Pointer to the tail of the filename list to populate */
+) { PROFILE(GENERATOR_SORT_FUNIT_BY_FILENAME);
+
+  while( funitl != NULL ) {
+
+    /* Only add modules that are not the $root "module" */
+    if( (funitl->funit->type == FUNIT_MODULE) && (strncmp( "$root", funitl->funit->name, 5 ) != 0) ) {
+
+      fname_link* fnamel = *head;
+
+      /* Search for functional unit filename in our filename list */
+      while( (fnamel != NULL) && (strcmp( fnamel->filename, funitl->funit->filename ) != 0) ) {
+        fnamel = fnamel->next;
+      }
+
+      /* If the filename link was not found, create a new link */
+      if( fnamel == NULL ) {
+
+        /* Allocate and initialize the filename link */
+        fnamel             = (fname_link*)malloc_safe( sizeof( fname_link ) );
+        fnamel->filename   = strdup_safe( funitl->funit->filename );
+        fnamel->next_funit = funitl->funit;
+        fnamel->head       = NULL;
+        fnamel->tail       = NULL;
+        fnamel->next       = NULL;
+
+        /* Add the new link to the list */
+        if( *head == NULL ) {
+          *head = *tail = fnamel;
+        } else {
+          (*tail)->next = fnamel;
+          *tail         = fnamel;
+        }
+
+      /*
+       Otherwise, if the filename was found, compare the start_line of the next_funit to this functional unit's first_line.
+       If the current functional unit's start_line is less, set next_funit to point to this functional unit.
+      */
+      } else {
+
+        if( fnamel->next_funit->start_line > funitl->funit->start_line ) {
+          fnamel->next_funit = funitl->funit;
+        }
+
+      }
+
+      /* Add the current functional unit to the filename functional unit list */
+      funit_link_add( funitl->funit, &(fnamel->head), &(fnamel->tail) );
+
+    }
+
+    /* Advance to the next functional unit */
+    funitl = funitl->next;
+
+  }
+ 
+  PROFILE_END;
+
+}
+
+/*!
+ \return Returns TRUE if next functional unit exists; otherwise, returns FALSE.
+
+ This function should be called whenever we see an endmodule of a covered module.
+*/
+static bool generator_set_next_funit(
+  fname_link* curr  /*!< Pointer to the current filename link being worked on */
+) { PROFILE(GENERATOR_SET_NEXT_FUNIT);
+
+  func_unit*  next_funit = NULL;
+  funit_link* funitl     = curr->head;
+
+  /* Find the next functional unit */
+  while( funitl != NULL ) {
+    if( (funitl->funit->start_line >= curr->next_funit->end_line) &&
+        ((next_funit == NULL) || (funitl->funit->start_line < next_funit->start_line)) ) {
+      next_funit = funitl->funit;
+    }
+    funitl = funitl->next;
+  }
+
+  /* Only change the next_funit if we found a next functional unit (we don't want this value to ever be NULL) */
+  if( next_funit != NULL ) {
+    curr->next_funit = next_funit;
+  }
+
+  PROFILE_END;
+
+  return( next_funit != NULL );
+
+}
+
+/*!
+ Deallocates all memory allocated for the given filename linked list.
+*/
+static void generator_dealloc_filename_list(
+  fname_link* head  /*!< Pointer to filename list to deallocate */
+) { PROFILE(GENERATOR_DEALLOC_FNAME_LIST);
+
+  fname_link* tmp;
+
+  while( head != NULL ) {
+    
+    tmp  = head;
+    head = head->next;
+
+    /* Deallocate the filename */
+    free_safe( tmp->filename, (strlen( tmp->filename ) + 1) );
+
+    /* Deallocate the functional unit list (without deallocating the functional units themselves) */
+    funit_link_delete_list( &(tmp->head), &(tmp->tail), FALSE );
+
+    /* Deallocate the link itself */
+    free_safe( tmp, sizeof( fname_link ) );
+
+  }
+
+  PROFILE_END;
+
+}
+
+static void generator_handle_stmt(
+  statement*    stmt,
+  char*         line,
+  unsigned int* curr_char
+) { PROFILE(GENERATOR_HANDLE_STMT);
+
+  printf( "HERE! line: %s, curr_char: %d\n", line, *curr_char );
+
+  PROFILE_END;
+
+}
+
+/*!
+ Takes the current line, outputting both original Verilog and injected covered code.
+*/
+static void generator_handle_line(
+  char*        line,      /*!< Current line */
+  unsigned int line_num,  /*!< Current line number */
+  fname_link*  fnamel     /*!< Pointer to current filename link */
+) { PROFILE(GENERATOR_HANDLE_LINE);
+
+  static func_iter  fi;
+  static statement* stmt      = NULL;
+  bool              end_found;
+  unsigned int      curr_char = 0;
+
+  printf( "In generator_handle_line, line: %s, line_num: %d, next_funit start: %d\n", line, line_num, fnamel->next_funit->start_line );
+
+  do {
+    end_found = FALSE;
+    if( (line_num >= fnamel->next_funit->start_line) && (line_num <= fnamel->next_funit->end_line) ) {
+      if( line_num == fnamel->next_funit->start_line ) {
+        func_iter_init( &fi, fnamel->next_funit, TRUE, FALSE );
+        printf( "func_iter initialized!\n" );
+        stmt = func_iter_get_next_statement( &fi );
+      }
+      while( (stmt != NULL) && (stmt->exp->line == line_num) ) {
+        generator_handle_stmt( stmt, line, &curr_char );
+        stmt = func_iter_get_next_statement( &fi );
+      }
+      if( line_num == fnamel->next_funit->end_line ) {
+        func_iter_dealloc( &fi );
+        end_found = generator_set_next_funit( fnamel );
+      }
+    }
+  } while( end_found );
+
+  PROFILE_END;
+
+}
+  
 /*!
  Generates an instrumented version of a given functional unit.
 */
-static void generator_output_funit(
-  func_unit* funit  /*!< Pointer to functional unit to output */
+static void generator_output_funits(
+  fname_link* head  /*!< Pointer to the head of the filename list */
 ) { PROFILE(GENERATOR_OUTPUT_FUNIT);
 
-  printf( "funit %s, type: %s\n", funit->name, get_funit_type( funit->type ) );
-
-  /* Only generate output for a given module (and it is not $root) */
-  if( (funit->type == FUNIT_MODULE) && (strncmp( "$root", funit->name, 5 ) != 0) ) {
+  while( head != NULL ) {
 
     FILE*        ofile;
     char         filename[4096];
     unsigned int rv;
 
-    printf( "HERE A!\n" );
-
-    /* TBD - The extension should probably be settable by the user */
-    rv = snprintf( filename, 4096, "covered/verilog/%s.v", funit->name );
+    rv = snprintf( filename, 4096, "covered/verilog/%s", get_basename( head->filename ) );
     assert( rv < 4096 );
-
-    printf( "Going to create %s\n", filename );
 
     /* Open the output file for writing */
     if( (ofile = fopen( filename, "w" )) != NULL ) {
 
       FILE* ifile;
 
-      printf( "module filename: %s\n", funit->filename );
-
       /* Open the original file for reading */
-      if( (ifile = fopen( funit->filename, "r" )) != NULL ) {
+      if( (ifile = fopen( head->filename, "r" )) != NULL ) {
 
         func_iter    fi;
         char*        line;
         unsigned int line_size;
         unsigned int line_num = 1;
 
-        /* Initialize the functional unit iterator */
-        func_iter_init( &fi, funit, TRUE, TRUE );
-
         /* Read in each line */
         while( util_readline( ifile, &line, &line_size ) ) {
-          if( (line_num >= funit->start_line) && (line_num <= funit->end_line) ) {
-            fprintf( ofile, "%s\n", line );
-          }
+          generator_handle_line( line, line_num, head );
           free_safe( line, line_size );
           line_num++;
         }
-
-        /* Deallocate functional unit iterator */
-        func_iter_dealloc( &fi );
 
         /* Close the input file */
         rv = fclose( ifile );
@@ -91,7 +263,7 @@ static void generator_output_funit(
 
       } else {
 
-        rv = snprintf( user_msg, USER_MSG_LENGTH, "Unable to open \"%s\" for reading", funit->filename );
+        rv = snprintf( user_msg, USER_MSG_LENGTH, "Unable to open \"%s\" for reading", head->filename );
         assert( rv < USER_MSG_LENGTH );
         fclose( ofile );
         Throw 0;
@@ -110,6 +282,8 @@ static void generator_output_funit(
 
     }
 
+    head = head->next;
+
   }
 
   PROFILE_END;
@@ -121,7 +295,9 @@ static void generator_output_funit(
 */
 void generator_output() { PROFILE(GENERATOR_OUTPUT);
 
-  funit_link* funitl = db_list[curr_db]->funit_head;
+  fname_link* fname_head;  /* Pointer to head of filename linked list */
+  fname_link* fname_tail;  /* Pointer to tail of filename linked list */
+  fname_link* fnamel;      /* Pointer to current filename link */
 
   /* Create the initial "covered" directory - TBD - this should be done prior to this function call */
   if( !directory_exists( "covered" ) ) {
@@ -145,18 +321,25 @@ void generator_output() { PROFILE(GENERATOR_OUTPUT);
     Throw 0;
   }
 
-  /* Iterate through the functional units, outputting the modules to their own files in the covered/verilog directory */
-  while( funitl != NULL ) {
-    generator_output_funit( funitl->funit );
-    funitl = funitl->next;
-  }
+  /* Create the filename list from the functional unit list */
+  generator_create_filename_list( db_list[curr_db]->funit_head, &fname_head, &fname_tail );
 
+  /* Iterate through the covered files, generating coverage output along with the design information */
+  generator_output_funits( fname_head );
+
+  /* Deallocate memory from filename list */
+  generator_dealloc_filename_list( fname_head );
   PROFILE_END;
 
 }
 
 /*
  $Log$
+ Revision 1.2  2008/11/26 05:34:48  phase1geo
+ More work on Verilog generator file.  We are now able to create the needed
+ directories and output a non-instrumented version of a module to the appropriate
+ directory.  Checkpointing.
+
  Revision 1.1  2008/11/25 23:53:07  phase1geo
  Adding files for Verilog generator functions.
 
