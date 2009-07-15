fe593a7c7a3ac67d23512fa1312e31293b167344
phase1geo <>
2009-07-15 03:37:37 +0000

    Enhancing the inlined_err1.pl diagnostic to run in both VCD and LXT modes of operation in IV regression runs.

--- diags/regress/inlined_err1.pl
+++ diags/regress/inlined_err1.pl
@@ -6,22 +6,34 @@
 
 require "../verilog/regress_subs.pl";
 
-# Initialize the diagnostic environment
-&initialize( "inlined_err1", 1, @ARGV );
+if( ($SIMULATOR eq "IV") && (($DUMPTYPE eq "LXT") || ($DUMPTYPE eq "VCD")) ) {
 
-# Perform initial score for inlined code coverage
-&runScoreCommand( "-t main -inline -v inlined_err1.v -o inlined_err1.cdd -D DUMP" );
+  # Initialize the diagnostic environment
+  &initialize( "inlined_err1", 1, @ARGV );
 
-# Compile and run this diagnostic using IV
-&runCommand( "iverilog -DDUMP covered/verilog/inlined_err1.v; ./a.out" );
+  # Perform initial score for inlined code coverage
+  &runScoreCommand( "-t main -inline -v inlined_err1.v -o inlined_err1.cdd -D DUMP" );
 
-# Perform second score for no inlined code coverage using VCD from inlined run
-&runScoreCommand( "-t main -vcd inlined_err1.vcd -v inlined_err1.v -o inlined_err1.cdd 2> inlined_err1.err" );
+  if( $DUMPTYPE eq "VCD" ) {
+    $dump_opt = "-vcd";
+    $iv_opt   = "";
+  } else {
+    $dump_opt = "-lxt";
+    $iv_opt   = "-lxt2";
+  }
 
-system( "cat inlined_err1.err" ) && die;
+  # Compile and run this diagnostic using IV
+  &runCommand( "iverilog -DDUMP covered/verilog/inlined_err1.v; ./a.out $iv_opt" );
 
-# Perform the file comparison checks
-&checkTest( "inlined_err1", 1, 1 );
+  # Perform second score for no inlined code coverage using VCD from inlined run
+  &runScoreCommand( "-t main $dump_opt inlined_err1.vcd -v inlined_err1.v -o inlined_err1.cdd 2> inlined_err1.err" );
+
+  system( "cat inlined_err1.err" ) && die;
+
+  # Perform the file comparison checks
+  &checkTest( "inlined_err1", 1, 1 );
+
+}
 
 exit 0;
 
