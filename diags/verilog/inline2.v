b07398ac9af205fc65f30b32e422fa35119a6ee8
phase1geo <>
2009-07-24 14:02:38 +0000

    Adding inline2 diagnostic to recreate bug 2826109.  Still need to fix bug.

---
+++ diags/verilog/inline2.v
@@ -0,0 +1,37 @@
+/*
+ Name:        inline2.v
+ Author:      Trevor Williams  (phase1geo@gmail.com)
+ Date:        07/24/2009
+ Purpose:     Verifies that an intermediate signal is not instantiated twice.
+*/
+
+module main;
+
+reg       clock;
+reg [1:0] a[0:1];
+reg       wen;
+reg [1:0] c;
+
+always @(posedge clock) begin
+  a[0] <= wen ? c : a[0];
+end
+
+initial begin
+`ifdef DUMP
+        $dumpfile( "inline2.vcd" );
+        $dumpvars( 0, main );
+`endif
+	c   = 2'b01;
+	wen = 1'b0;
+	@(posedge clock);
+	wen = 1'b1;
+        #5;
+        $finish;
+end
+
+initial begin
+	clock = 1'b0;
+	forever #(2) clock = ~clock;
+end
+
+endmodule
