2f43781c91d765b29c2b88c0a57648c35bccf225
phase1geo <>
2009-10-27 13:58:29 +0000

    More verilator regression updates.

--- diags/verilog/regress_subs.pl
+++ diags/verilog/regress_subs.pl
@@ -478,6 +478,9 @@ sub convertCfg {
       $line =~ s/[0-9a-zA-Z_\.]+\.(vcd|dump)/covered_vpi.v/g;
     } elsif( ($type eq "inline") || ($type eq "inline -inline-comb-depth 1") ) {
       $line =~ s/[0-9a-zA-Z_\.]+\.(vcd|dump)//g;
+      if( $using_verilator == 1 ) {
+        $line .= " -inline-metrics ltmcfa";
+      }
     }
     print OFILE $line;
   }
