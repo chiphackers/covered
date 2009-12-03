6abd6340af15904d305262f9730e9a3a92ff6b66
phase1geo <>
2009-12-03 00:26:32 +0000

    More updates.  Checkpointing.

--- src/covered_verilator.h
+++ src/covered_verilator.h
@@ -1,9 +1,17 @@
 #ifndef __COVERED_VERILATOR_H__
 #define __COVERED_VERILATOR_H__
 
+#ifdef __cplusplus
+extern "C" {
+  void db_add_line_coverage( uint32_t, uint32_t );
+  int db_read( const char*, int );
+  void bind_perform( int, int );
+}
+#else
 extern void db_add_line_coverage( uint32_t, uint32_t );
 extern int db_read( const char*, int );
 extern void bind_perform( int, int );
+#endif
 
 inline void covered_line( uint32_t inst_index, uint32_t expr_index ) {
   db_add_line_coverage( inst_index, expr_index );
