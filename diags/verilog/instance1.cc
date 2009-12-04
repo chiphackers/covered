0923eba1d8a5675f40f4740b108f10fc7b03d6db
phase1geo <>
2009-12-04 00:22:26 +0000

    Adding instance1 Verilator version.  Cleaning up old lib directory.

---
+++ diags/verilog/instance1.cc
@@ -0,0 +1,47 @@
+#include <verilated.h>             // Defines common routines
+#include "Vinstance1.h"           // From Verilating "add1.v"
+#include <SpTraceVcdC.h>           // Trace file format header (from SystemPerl)
+#ifdef COVERED_INLINED
+#include "covered_verilator.h"
+#endif
+
+Vinstance1 *top;                  // Instantiation of module
+
+unsigned int main_time = 0;        // Current simulation time
+
+double sc_time_stamp () {          // Called by $time in Verilog
+  return main_time;
+}
+
+int main() {
+
+  top = new Vinstance1;           // Create instance
+
+  Verilated::traceEverOn( true );  // Verilator must compute traced signals
+  SpTraceVcdCFile* tfp = new SpTraceVcdCFile;
+  top->trace( tfp, 99 );           // Trace 99 levels of hierarchy
+  tfp->open( "instance1.vcd" );   // Open the dump file
+
+#ifdef COVERED_INLINED
+  covered_initialize( "../instance1.cdd" );
+#endif
+
+  top->verilatorclock = 0;
+
+  while( !Verilated::gotFinish() ) {
+    top->verilatorclock = (main_time % 2);   // Toggle clock
+    top->eval();                   // Evaluate model
+    tfp->dump( main_time );        // Create waveform trace for this timestamp
+    main_time++;                   // Time passes...
+  }
+
+  top->final();                    // Done simulating
+
+#ifdef COVERED_INLINED
+  covered_close( "../instance1.cdd");
+#endif
+
+  tfp->close();
+
+}
+
---
+++ diags/verilog/instance1.verilator.v
@@ -0,0 +1,50 @@
+module main( input verilatorclock );
+
+reg    a, b;
+
+/* coverage off */
+always @(posedge verilatorclock) begin
+  if( $time == 1 ) begin
+    a <= 1'b0;
+    b <= 1'b0;
+  end
+  else if( $time == 5 ) begin
+    a <= 1'b1;
+  end
+  else if( $time == 9 ) begin
+    b <= 1'b1;
+  end
+  else if( $time == 13 ) begin
+    a <= 1'b0;
+  end
+  else if( $time == 17 ) $finish;
+end
+/* coverage off */
+
+depth1 inst0( a, b );
+
+endmodule
+
+module depth1( a1, b1 );
+
+input    a1;
+input    b1;
+
+wire     c1;
+
+assign c1 = a1 & b1;
+
+depth2 inst1( a1, b1 );
+
+endmodule
+
+module depth2( a2, b2 );
+
+input    a2;
+input    b2;
+
+wire     c2;
+
+assign c2 = a2 ^ b2;
+
+endmodule
